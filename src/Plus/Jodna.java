727a782157e28e23280f4acf2193306d332a5923
Amitna95 <amitnabind751@gmail.com>
2025-01-16 05:57:45 +0530

    New file has been changed

--- src/Plus/Jodna.java
+++ src/Plus/Jodna.java
@@ -7,6 +7,16 @@ public class Jodna {
 		int j = 222;
 		int z = i+j;
 		System.out.println(z);
+		
+		int a = 333;
+		int b = 444;
+		int c = a+b;
+		System.out.println(c);
+		
+		int d = 333;
+		int e = 444;
+		int f = d+e;
+		System.out.println(f);
 
 	}
 
